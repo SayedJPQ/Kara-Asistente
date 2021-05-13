be6f2707e83e0f59ca886c4dec55a93ab1fc8d3a
SayedJPQ <sayedjquiros@gmail.com>
2021-05-12 20:32:27 -0600

    Optimizacion incompleta

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -36,126 +36,165 @@ Partial Class Form1
         Me.TrackBarVelocidad = New System.Windows.Forms.TrackBar()
         Me.TrackBarVolumen = New System.Windows.Forms.TrackBar()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.TrackBarVelocidad, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TrackBarVolumen, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(7, 81)
+        Me.TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
+        Me.TextBox1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 81)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(109, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(178, 29)
         Me.TextBox1.TabIndex = 0
         '
         'Respuesta
         '
-        Me.Respuesta.Location = New System.Drawing.Point(137, 81)
+        Me.Respuesta.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Respuesta.Location = New System.Drawing.Point(196, 81)
+        Me.Respuesta.Multiline = True
         Me.Respuesta.Name = "Respuesta"
-        Me.Respuesta.Size = New System.Drawing.Size(115, 20)
+        Me.Respuesta.Size = New System.Drawing.Size(190, 47)
         Me.Respuesta.TabIndex = 1
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(392, 81)
+        Me.TextBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
+        Me.TextBox3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(523, 81)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(142, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(162, 29)
         Me.TextBox3.TabIndex = 2
         '
         'Traducido
         '
-        Me.Traducido.Location = New System.Drawing.Point(553, 81)
+        Me.Traducido.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Traducido.Location = New System.Drawing.Point(712, 81)
         Me.Traducido.Name = "Traducido"
-        Me.Traducido.Size = New System.Drawing.Size(141, 20)
+        Me.Traducido.Size = New System.Drawing.Size(170, 29)
         Me.Traducido.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(93, 35)
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(175, 35)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(23, 13)
+        Me.Label1.Size = New System.Drawing.Size(37, 21)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Bot"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(515, 35)
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(636, 35)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.Size = New System.Drawing.Size(84, 21)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Traductor"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(137, 257)
+        Me.TextBox2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(12, 189)
+        Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(717, 143)
         Me.TextBox2.TabIndex = 6
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(96, 134)
+        Me.Button1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(127, 134)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(64, 27)
+        Me.Button1.Size = New System.Drawing.Size(110, 32)
         Me.Button1.TabIndex = 7
         Me.Button1.Text = "Realizar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(504, 134)
+        Me.Button2.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(645, 129)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(64, 27)
+        Me.Button2.Size = New System.Drawing.Size(106, 37)
         Me.Button2.TabIndex = 8
         Me.Button2.Text = "Traducir"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(165, 296)
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(323, 353)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(64, 27)
+        Me.Button3.Size = New System.Drawing.Size(92, 53)
         Me.Button3.TabIndex = 9
         Me.Button3.Text = "Hablar"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(781, 338)
+        Me.Button4.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(830, 386)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(102, 41)
+        Me.Button4.Size = New System.Drawing.Size(102, 70)
         Me.Button4.TabIndex = 10
         Me.Button4.Text = "Cerrar programa"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'TrackBarVelocidad
         '
-        Me.TrackBarVelocidad.Location = New System.Drawing.Point(553, 309)
+        Me.TrackBarVelocidad.Location = New System.Drawing.Point(31, 411)
         Me.TrackBarVelocidad.Name = "TrackBarVelocidad"
         Me.TrackBarVelocidad.Size = New System.Drawing.Size(206, 45)
         Me.TrackBarVelocidad.TabIndex = 11
         '
         'TrackBarVolumen
         '
-        Me.TrackBarVolumen.Location = New System.Drawing.Point(328, 309)
+        Me.TrackBarVolumen.Location = New System.Drawing.Point(514, 411)
         Me.TrackBarVolumen.Name = "TrackBarVolumen"
         Me.TrackBarVolumen.Size = New System.Drawing.Size(206, 45)
         Me.TrackBarVolumen.TabIndex = 12
         '
         'ComboBox1
         '
+        Me.ComboBox1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(674, 180)
+        Me.ComboBox1.Location = New System.Drawing.Point(735, 189)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(208, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(208, 29)
         Me.ComboBox1.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(577, 355)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 21)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Volumen"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(107, 363)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Label4"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(909, 391)
+        Me.ClientSize = New System.Drawing.Size(944, 468)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.TrackBarVolumen)
         Me.Controls.Add(Me.TrackBarVelocidad)
@@ -193,4 +232,6 @@ Partial Class Form1
     Friend WithEvents TrackBarVelocidad As TrackBar
     Friend WithEvents TrackBarVolumen As TrackBar
     Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- Form3.vb
+++ Form3.vb
@@ -1,19 +1,19 @@
 ﻿Imports System.Speech
 Imports SpeechLib
 Imports System.Speech.Recognition
+Imports System.Runtime.InteropServices
+Imports System.Speech.Recognition.SrgsGrammar
+Imports System.Threading
+Imports System.Globalization
 Public Class Form3
     Dim speech As SpVoice = New SpVoice()
     Dim WithEvents Rec As New Recognition.SpeechRecognitionEngine
     Dim synth As New Synthesis.SpeechSynthesizer
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Try
-        Dim Prueba As New System.Globalization.CultureInfo("es-MX")
-        Dim Gramatica As New Recognition.SrgsGrammar.SrgsDocument
-        Dim Regla As New Recognition.SrgsGrammar.SrgsRule("Hola")
-        Dim Lista As New Recognition.SrgsGrammar.SrgsOneOf("Hola", "Como estas?")
-        Regla.Add(Lista)
-        Gramatica.Rules.Add(Regla)
-        Gramatica.Root = Regla
+        Thread.CurrentThread.CurrentUICulture = New CultureInfo("sp-MX")
+        Dim Gramatica As New GrammarBuilder()
+        Gramatica.Append(New Choices(System.IO.File.ReadAllLines("C:\Users\PC0\Desktop\Kara\ComandosVoz.txt")))
         Rec.SetInputToDefaultAudioDevice()
         Rec.LoadGrammar(New Recognition.Grammar(Gramatica))
         Rec.RecognizeAsync()
