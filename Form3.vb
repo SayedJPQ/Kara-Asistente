60dd53ed819a7a227d324d737311326ac165446c
SayedJPQ <sayedjquiros@gmail.com>
2021-05-15 09:38:47 -0600

    Comandos de voz funcional

--- Form3.vb
+++ Form3.vb
@@ -7,24 +7,30 @@ Imports System.Threading
 Imports System.Globalization
 Public Class Form3
     Dim speech As SpVoice = New SpVoice()
-    Dim WithEvents Rec As New Recognition.SpeechRecognitionEngine
+    Dim WithEvents Rec As New Recognition.SpeechRecognitionEngine(New System.Globalization.CultureInfo("es-MX"))
     Dim synth As New Synthesis.SpeechSynthesizer
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Try
-        Thread.CurrentThread.CurrentUICulture = New CultureInfo("sp-MX")
-        Dim Gramatica As New GrammarBuilder()
-        Gramatica.Append(New Choices(System.IO.File.ReadAllLines("C:\Users\PC0\Desktop\Kara\ComandosVoz.txt")))
-        Rec.SetInputToDefaultAudioDevice()
-        Rec.LoadGrammar(New Recognition.Grammar(Gramatica))
-        Rec.RecognizeAsync()
+        Thread.CurrentThread.CurrentCulture = New CultureInfo("es-MX")
+            Thread.CurrentThread.CurrentUICulture = New CultureInfo("es-MX")
+            Dim Gramatica As New GrammarBuilder()
+            Gramatica.Culture = Rec.RecognizerInfo.Culture()
+            Gramatica.Append(New Choices(System.IO.File.ReadAllLines("C:\Users\PC0\Desktop\Kara\ComandosVoz.txt")))
+            Rec.LoadGrammar(New Recognition.Grammar(Gramatica))
+            Rec.SetInputToDefaultAudioDevice()
+            Rec.RecognizeAsync()
         'Catch ex As Exception
         'MsgBox(ex.Message)
         'End Try
     End Sub
     Private Sub Reco_Recognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles Rec.SpeechRecognized
+        speech.Rate = 0
+        speech.Volume = 80
         Select Case e.Result.Text
             Case "Hola"
                 speech.Speak("Hola", SpeechVoiceSpeakFlags.SVSFlagsAsync)
+            Case "Adios"
+                speech.Speak("Adios", SpeechVoiceSpeakFlags.SVSFlagsAsync)
             Case Else
                 speech.Rate = 0
                 speech.Volume = 80
@@ -32,7 +38,9 @@ Public Class Form3
         End Select
     End Sub
     Private Sub Reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs)
-        Rec.RecognizeAsync()
+        Do
+            Rec.RecognizeAsync()
+        Loop
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
